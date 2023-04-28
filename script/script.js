3a4c07ec03823c47bff5ac60f40cac4869c49607
Vanounais <89648070+Vanounais@users.noreply.github.com>
2023-04-28 10:57:36 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const appelAnim = gsap.timeline()
+    .to('.fleche.trois', { opacity: "0", ease: "power2.inOut", duration: "1"}) 
+    .to('.fleche.deux', { opacity: "0", ease: "power2.inOut", duration: "1"}, "-=0.6")
+    .to('.fleche.un', { opacity: "0", ease: "power2.inOut", duration: "1"}, "-=0.6")
+    .to('.fleche.trois', { opacity: "1", ease: "power2.inOut", duration: "1"}, "<")
+    .to('.fleche.deux', { opacity: "1", ease: "power2.inOut", duration: "1"}, "-=0.6")
+    .to('.fleche.un', { opacity: "1", ease: "power2.inOut", duration: "1"}, "-=0.6")
+    .repeat(-1)
+
+window.addEventListener("scroll",() => {
+    const body = document.querySelector("body")
+    body.classList.add("is-scrolling");
+});
