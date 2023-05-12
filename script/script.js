6aa5f57a6d42bc0f44d3488e0a59e24ced26fa01
Vanounais <89648070+Vanounais@users.noreply.github.com>
2023-05-12 06:57:25 -0400

    fewfe

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* appel a l'action */
 const appelAnim = gsap.timeline()
     .to('.fleche.trois', { opacity: "0", ease: "power2.inOut", duration: "1"}) 
     .to('.fleche.deux', { opacity: "0", ease: "power2.inOut", duration: "1"}, "-=0.6")
@@ -7,13 +8,28 @@ const appelAnim = gsap.timeline()
     .to('.fleche.un', { opacity: "1", ease: "power2.inOut", duration: "1"}, "-=0.6")
     .repeat(-1)
 
+
+/* scroll spritesheet */
+let isScrolling
 window.addEventListener("scroll",() => {
     const body = document.querySelector("body")
     body.classList.add("is-scrolling");
 });
+let scrollend = gsap.to('chapt.no4-5', {
+    scrollTrigger: {
+        trigger: 'chapt.no4-5',
+        onUpdate: (e) => {
+            window.clearTimeout(isScrolling)
+            timeOut = setTimeout(function () {
+                document.querySelector("body").classList.remove("is-scrolling");
+            }, 250)
+        }
+    }
+})
 
 gsap.registerPlugin(ScrollTrigger);
 
+/* chapitre 1 */
 const chapt1 = gsap.timeline({
     scrollTrigger: {
         pin: true,
@@ -25,11 +41,13 @@ const chapt1 = gsap.timeline({
     }
 });
     chapt1
+    .to('#chapitre-1 .text', {opacity: 1},"<")
     .to('#chapitre-1 .voiture', {x: '550%'},"<")
     .to('#chapitre-1 .voiture', {y: '200%', x: '600%'})
     .to('#chapitre-1 .voiture', {rotation: 180},"<")
     .to('#chapitre-1 .mongolfiere', {y: '-150vh',x: '-80%'})
 
+/* chapitre 2 */
 const chapt2 = gsap.timeline({
     scrollTrigger: {
         pin: true,
@@ -41,10 +59,12 @@ const chapt2 = gsap.timeline({
     }
 });
     chapt2
+    .to('#chapitre-2 .text', {opacity: 1},"<")
     .to('#chapitre-2 .mongolfiere', {y: '-70vh', x: '50%', ease:'power2.out'}, "<")
     .to('#chapitre-2 .mongolfiere', {x: '500%', ease: 'power2.inOut', duration: 2})
     .to('#chapitre-2 .nuages', {x: '-100%', duration: 2},"0")
         
+/* chapitre 3 */
 const chapt3 = gsap.timeline({
     scrollTrigger: {
         pin: true,
@@ -56,80 +76,86 @@ const chapt3 = gsap.timeline({
     }
 });
     chapt3
-    .to('#chapitre-3 .mongolfiere', {x: '200%', duration: 2})
+    .to('#chapitre-3 .text', {opacity: 1},"<")
+    .to('#chapitre-3 .mongolfiere', {x: '200%', duration: 2},"<")
     .to('#chapitre-3 .mongolfiere', {y: '-2vh', x: '210%'})
     .to('#chapitre-3 .mongolfiere', {y: '1vh', x: '205%', ease: 'power1.inOut'})
     .to('#chapitre-3 .mongolfiere', {y: '-1vh', x: '215%', ease: 'power1.inOut'})
     .to('#chapitre-3 .mongolfiere', {y: '-3vh', x: '207%', ease: 'power1.inOut'})
     .to('#chapitre-3 .mongolfiere', {y: '70vh', x: '300%', rotation: 50, ease: 'power2.in', duration: 1})
 
-    const chapt4_5 = gsap.timeline({
-        scrollTrigger: {
-            pin: true,
-            scrub: 1,
-            markers: true,
-            start: 'top 0%',
-            end: 'bottom -300%',
-            trigger: '.chapt.no4-5',
-        }
-    });
-        chapt4_5
-        .to('#chapitre-4-5 .mongolfiere', { rotation: 20, duration: 0})
-        .to('#chapitre-4-5 .mongolfiere', {y: '150vh', x: '200%', rotation: 90, duration: 1})
-        .to('#chapitre-4-5 .vague-1', {y: '-50vh'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-35vh'},'<+0.2')
-        .to('#chapitre-4-5 .vague-3', {y: '-20vh'},'<+0.2')
-        .to('#chapitre-4-5 .bateau', {y: '-75vh', x: '20%', rotation: 10, ease: 'power1.inOut'},'<-0.2')
-        .to('#chapitre-4-5 .bateau', {y: '-73vh',rotation: 0, ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .vague-1', {y: '-52vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-32vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-3', {y: '-22vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .bateau', {rotation: -5, ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .vague-1', {y: '-51vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-36vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-3', {y: '-19vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .bateau', {y: '-73vh',rotation: 0, ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .vague-1', {y: '-53vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-31vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-3', {y: '-21vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .bateau', {y: '-75vh', rotation: 10, ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .vague-1', {y: '-50vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-35vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-3', {y: '-20vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .bateau', {y: '-0vh', rotation: 50, ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .vague-1', {y: '-51vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-36vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-3', {y: '-19vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .background', {y: '-150vh', ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .vague-1', {y: '-201vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-2', {y: '-185vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .vague-3', {y: '-169vh', ease: 'power1.inOut'},'<')
-        .to('#chapitre-4-5 .sous-marins', {x: '100%', y: '5vh',rotation: 20, ease: 'power1.inOut'})
-        .to('#chapitre-4-5 .sous-marins', {x: '200%', y: '90vh',rotation: 70, ease: 'power1.inOut', duration: 3})
-        .to('#chapitre-4-5 .background', {y: '-300vh', ease: 'power1.inOut', duration: 3},"<")
-        .to('#chapitre-4-5 .bulle-1', {y: '-140vh', ease: 'power1.in', duration: 2},"<")
-        .to('#chapitre-4-5 .poisson-rouge', {y: '-40vh', x: '-600%', ease: 'power1.in', duration: 2},"<")
-        .to('#chapitre-4-5 .bulle-3', {y: '-140vh', ease: 'power1.in', duration: 2},"<0.5")
-        .to('#chapitre-4-5 .poisson-jaune', {y: '-40vh', x: '600%', ease: 'power1.in', duration: 2},"<")
-        .to('#chapitre-4-5 .bulle-2', {y: '-140vh', ease: 'power1.in', duration: 2},"<0.5")
-        .to('#chapitre-4-5 .poisson-vert', {y: '-40vh', x: '600%', ease: 'power1.in', duration: 2},"<")
+/* chapitre 4-5 */
+const chapt4_5 = gsap.timeline({
+    scrollTrigger: {
+        pin: true,
+        scrub: 1,
+        markers: true,
+        start: 'top 0%',
+        end: 'bottom -300%',
+        trigger: '.chapt.no4-5',
+    }
+});
+    chapt4_5
+    .to('#chapitre-4-5 .text', {opacity: 1},"<")
+    .to('#chapitre-4-5 .mongolfiere', { rotation: 20, duration: 0},'<')
+    .to('#chapitre-4-5 .mongolfiere', {y: '150vh', x: '200%', rotation: 90, duration: 1})
+    .to('#chapitre-4-5 .vague-1', {y: '-50vh'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-35vh'},'<+0.2')
+    .to('#chapitre-4-5 .vague-3', {y: '-20vh'},'<+0.2')
+    .to('#chapitre-4-5 .bateau', {y: '-75vh', x: '20%', rotation: 10, ease: 'power1.inOut'},'<-0.2')
+    .to('#chapitre-4-5 .bateau', {y: '-73vh',rotation: 0, ease: 'power1.inOut'})
+    .to('#chapitre-4-5 .vague-1', {y: '-52vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-32vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-3', {y: '-22vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .bateau', {rotation: -5, ease: 'power1.inOut'})
+    .to('#chapitre-4-5 .vague-1', {y: '-51vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-36vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-3', {y: '-19vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .bateau', {y: '-73vh',rotation: 0, ease: 'power1.inOut'})
+    .to('#chapitre-4-5 .vague-1', {y: '-53vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-31vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-3', {y: '-21vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .bateau', {y: '-75vh', rotation: 10, ease: 'power1.inOut'})
+    .to('#chapitre-4-5 .vague-1', {y: '-50vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-35vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-3', {y: '-20vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .bateau', {y: '-0vh', rotation: 50, ease: 'power1.inOut'})
+    .to('#chapitre-4-5 .vague-1', {y: '-51vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-36vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-3', {y: '-19vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .background', {y: '-150vh', ease: 'power1.inOut'})
+    .to('#chapitre-4-5 .vague-1', {y: '-201vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-2', {y: '-185vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .vague-3', {y: '-169vh', ease: 'power1.inOut'},'<')
+    .to('#chapitre-4-5 .text', {y: '-150vh', ease: 'power1.inOut'},"<")
+    .to('#chapitre-4-5 .text-2', {opacity: 1})
+    .to('#chapitre-4-5 .sous-marins', {x: '100%', y: '5vh',rotation: 20, ease: 'power1.inOut'},"<")
+    .to('#chapitre-4-5 .sous-marins', {x: '200%', y: '90vh',rotation: 70, ease: 'power1.inOut', duration: 3})
+    .to('#chapitre-4-5 .background', {y: '-300vh', ease: 'power1.inOut', duration: 3},"<")
+    .to('#chapitre-4-5 .bulle-1', {y: '-140vh', ease: 'power1.in', duration: 2},"<")
+    .to('#chapitre-4-5 .poisson-rouge', {y: '-40vh', x: '-600%', ease: 'power1.in', duration: 2},"<")
+    .to('#chapitre-4-5 .bulle-3', {y: '-140vh', ease: 'power1.in', duration: 2},"<0.5")
+    .to('#chapitre-4-5 .poisson-jaune', {y: '-40vh', x: '600%', ease: 'power1.in', duration: 2},"<")
+    .to('#chapitre-4-5 .bulle-2', {y: '-140vh', ease: 'power1.in', duration: 2},"<0.5")
+    .to('#chapitre-4-5 .poisson-vert', {y: '-40vh', x: '600%', ease: 'power1.in', duration: 2},"<")
         
+/* chapitre 6 */
+let hauteurTotale = document.querySelector("#chapitre-6").scrollWidth;
+console.log(hauteurTotale)
+const chapt6 = gsap.timeline({
+    scrollTrigger: {
+        pin: true,
+        scrub: 1,
+        markers: true,
+        start: 'top 0%',
+        end: 'bottom -300%',
+        trigger: '.chapt.no6',
+    }
+});
+    chapt6
+    .to('#chapitre-6 .text', {opacity: 1})
+    .to('#chapitre-6 .fusee', {rotate: 90, duration: 0},'<')
+    .to('#chapitre-6 .planete-1', {x: hauteurTotale*-0.35},'<')
+    .to('#chapitre-6 .planete-2', {x: hauteurTotale*-0.60},'<')
+    .to('#chapitre-6 .fusee', {x: "1250%", y: "15vh", rotate: 100, ease: "power1.inOut"},'<0.1')
 
-        let hauteurTotale = document.querySelector("#chapitre-6").scrollHeight;
-        console.log(hauteurTotale)
-
-        const chapt6 = gsap.timeline({
-            scrollTrigger: {
-                pin: true,
-                scrub: 1,
-                markers: true,
-                start: 'top 0%',
-                end: 'bottom -300%',
-                trigger: '.chapt.no6',
-            }
-        });
-            chapt6
-            .to('#chapitre-6 .fusee', {rotate: 90, duration: 0},0)
-            .to('#chapitre-6 .planete-1', {x: hauteurTotale*-0.35})
-            .to('#chapitre-6 .planete-2', {x: hauteurTotale*-1.10},'<')
-            .to('#chapitre-6 .fusee', {x: "1250%", y: "15vh", rotate: 100, ease: "power1.inOut"},'<0.1')
